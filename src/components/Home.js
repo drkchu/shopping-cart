d14f2df754620b39fe31349a148e59698f587761
drkchu <derekdbchu@gmail.com>
2024-07-31 11:32:46 -0700

    feat: set up routing, still broken though cuz not all components are done :D

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Enjoy your meal!</h1>
+      <p>Discover our specially curated selection of things that Derek (the creator of this site) is craving.</p>
+      <button>Shop now</button>
+    </div>
+    
+    // TODO: Add a scrolling window to display the items available in the shop
+  );
+};
+
+export default Home;
